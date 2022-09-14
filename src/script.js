e1271c5d99660783ec0439672d9a821a95eb0d54
onifadjosh <onifadjosh@gmail.com>
2022-09-14 01:37:48 +0100

    repairing links

--- src/script.js
+++ src/script.js
@@ -1,22 +1,22 @@
-// document.addEventListener("DOMContentLoaded", () => {
-//     function counter(id, start, end, duration) {
-//      let obj = document.getElementById(id),
-//       current = start,
-//       range = end - start,
-//       increment = end > start ? 1 : -1,
-//       step = Math.abs(Math.floor(duration / range)),
-//       timer = setInterval(() => {
-//        current += increment;
-//        obj.textContent = current;
-//        if (current == end) {
-//         clearInterval(timer);
-//        }
-//       }, step);
-//     }
-//     counter("count1", 200, 400, 400);
-//     counter("count2", 50, 150, 150);
-//     counter("count3", 40, 192, 192);
-//    });
+document.addEventListener("DOMContentLoaded", () => {
+    function counter(id, start, end, duration) {
+     let obj = document.getElementById(id),
+      current = start,
+      range = end - start,
+      increment = end > start ? 1 : -1,
+      step = Math.abs(Math.floor(duration / range)),
+      timer = setInterval(() => {
+       current += increment;
+       obj.textContent = current;
+       if (current == end) {
+        clearInterval(timer);
+       }
+      }, step);
+    }
+    counter("count1", 200, 400, 400);
+    counter("count2", 50, 150, 150);
+    counter("count3", 40, 192, 192);
+   });
   
 
    
