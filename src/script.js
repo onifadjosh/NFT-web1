8d084742009aceb763efb6a5367436c0a53624fd
onifadjosh <onifadjosh@gmail.com>
2022-09-14 01:22:55 +0100

    nft web for a friend

---
+++ src/script.js
@@ -0,0 +1,57 @@
+// document.addEventListener("DOMContentLoaded", () => {
+//     function counter(id, start, end, duration) {
+//      let obj = document.getElementById(id),
+//       current = start,
+//       range = end - start,
+//       increment = end > start ? 1 : -1,
+//       step = Math.abs(Math.floor(duration / range)),
+//       timer = setInterval(() => {
+//        current += increment;
+//        obj.textContent = current;
+//        if (current == end) {
+//         clearInterval(timer);
+//        }
+//       }, step);
+//     }
+//     counter("count1", 200, 400, 400);
+//     counter("count2", 50, 150, 150);
+//     counter("count3", 40, 192, 192);
+//    });
+  
+
+   
+
+   const hamburger = document.querySelector(".hamburger");
+   const navMenu = document.querySelector(".nav-menu");
+   
+   hamburger.addEventListener("click", () => {
+     hamburger.classList.toggle("active");
+     navMenu.classList.toggle("active");
+   })
+   
+   document.querySelectorAll("nav-link").forEach(n => n.
+     addEventListener("click", () => {
+       hamburger.classList.remove("active");
+       navContent.classList.remove("active");
+     }))
+
+     var slideIndex = 0;
+     showSlides();
+     
+     function showSlides() {
+       var i;
+       var slides = document.getElementsByClassName("mySlides");
+       var dots = document.getElementsByClassName("dot");
+       for (i = 0; i < slides.length; i++) {
+         slides[i].style.display = "none";  
+       }
+       slideIndex++;
+       if (slideIndex > slides.length) {slideIndex = 1}    
+       for (i = 0; i < dots.length; i++) {
+         dots[i].className = dots[i].className.replace(" active2", "");
+       }
+       slides[slideIndex-1].style.display = "flex";  
+       dots[slideIndex-1].className += " active2";
+       setTimeout(showSlides, 5000); // Change slide every 5 seconds
+     }
+    
---
+++ tailwind.config.js
@@ -0,0 +1,41 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./*.{html,js}"],
+  theme: {
+    extend: {
+      fontFamily: {
+       
+        'display': ['Rubik'],
+        'about': ['Comic Neue']
+          
+      },
+
+      backgroundImage: {
+        'hero': "url('./images/images (3).jpeg')",
+      },
+
+      colors: {
+        'primcol': '#a1d283',
+        'seccol': '#000000',
+        'tetcol': '#2B076E',
+        'faded-white': 'rgba(255, 255, 255, 0.05)',
+        'faded-white': 'rgba(255, 255, 255, 0.05)',
+      },
+    },
+
+    screens: {
+      'sm': '280px',
+      // => @media (min-width: 280px) { ... }
+
+      'md': '640px',
+      // => @media (min-width: 640px) { ... }
+
+      'lg': '1024px',
+      // => @media (min-width: 1024px) { ... }
+
+      'xl': '1280px',
+      // => @media (min-width: 1280px) { ... }
+    },
+  },
+  plugins: [],
+}
